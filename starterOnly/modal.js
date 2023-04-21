cb1c4b375b34b01c5c4fc20a7e170f0ecaa5e3a4
olaf <juliendupdupdup@gmail.com>
2023-04-21 03:20:22 +0000

    issue#4: ajouter confirmation quand envoi réussi

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,235 +12,275 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// event click sur btn
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// fonction ouverture de la modal
 function launchModal() {
   modalbg.style.display = "block";
 }
-// -------------------------
-// issue#1: fermer la modale
-// -------------------------
 
 // DOM Elements
 const closeBtn = document.querySelector(".close");
 
-// close modal event
+// event click sur close
 closeBtn.addEventListener("click", closeModal);
 
-// close modal form
+// fonction fermeture de la modal
 function closeModal() {
   modalbg.style.display = "none";
 }
-// -------------------------
 
-// ------------------------------------------
-// issue#2: Implémenter entrées du formulaire
-// ------------------------------------------
+// comportement du bouton submit
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  // empeche la modal de se fermer au click
+  e.preventDefault();
+  // lance la fonction de test des inputs
+  testInputsValidity();
+});
 
-// start livecoding with emeric
-
+// récupère les données entrées dans un champ en fonction de son id
 function getValue(inputId) {
   const input = document.getElementById(inputId);
   return input.value;
 }
+// stockage des id des inputs avec leur messages d'erreur associés
+// TODO ajouter une key isValid pour controler la validité de l'input ? utilisation d'un constructor
+const inputsIds = [
+  {
+    id: "first",
+    errorMessage: "Veuillez entrer 2 caractères ou plus",
+  },
+  {
+    id: "last",
+    errorMessage: "Veuillez entrer 2 caractères ou plus",
+  },
+  {
+    id: "email",
+    errorMessage: "Veuillez entrer une adresse e-mail valide",
+  },
+  {
+    id: "birthdate",
+    errorMessage:
+      "Veuillez entrer une date de naissance valide (mois/jour/année)",
+  },
+  {
+    id: "quantity",
+    errorMessage: "Veuillez entrer un nombre en chiffres",
+  },
+  {
+    id: "location1",
+    errorMessage: "Veuillez choisir un tournoi",
+  },
+  {
+    id: "checkbox1",
+    errorMessage: "Veuillez accepter les conditions d'utilisations",
+  },
+];
 
-// const inputIds = ["first", "last", ""]
-
-const inputsIds = [{ id: "first", errorMessage: "mon message" }];
-
-for (let index = 0; index < inputsIds.length; index++) {
-  const input = inputsIds[index];
-  let inputValue = getValue(input.id);
-  console.log(inputValue);
-  // utiliser switch
+// afficher le message d'erreur et la classe CSS erreur
+function showError(element, message) {
+  element.setAttribute("data-error-visible", "true");
+  // TODO comprendre comment la classe : data-error-visible="true"
+  element.setAttribute("data-error", message);
 }
 
-const formInputs = {
-  first: [
-    firstIsValid,
-    "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
-  ],
-  last: [
-    lastIsValid,
-    "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
-  ],
-  email: [emailIsValid, "Veuillez entrer une adresse e-mail valide."],
-  birthdate: [birthdateIsValid, "Vous devez entrer votre date de naissance."],
-  quantity: [quantityIsValid, "Veuillez entrer un nombre."],
-  location: [locationIsValid, "Vous devez choisir une option."],
-  conditions: [
-    conditionsIsValid,
-    "Vous devez accepter les termes et conditions.",
-  ],
-};
-
-// end livecoding with emeric
-
-// get first
-let first = document.getElementById("first");
-
-// get first value
-let firstValue = first.value;
-
-// get first value bis
-let firstValue2 = document.getElementById("first").value;
-
-// get first value length
-let firstValueLength = firstValue.length;
-
-// check if a value length is valid
-function lengthIsLongEnough(valueLength) {
-  return valueLength >= 2;
+// masquer le message d'erreur et la classe CSS erreur
+function hideError(element) {
+  element.setAttribute("data-error-visible", "false");
+  element.removeAttribute("data-error");
 }
 
-// check if first value length is valid
-let firstIsValid = lengthIsLongEnough(firstValueLength);
+// variable de l'état de la validité des inputs
+let firstIsValid,
+  lastIsValid,
+  emailIsValid,
+  birthdateIsValid,
+  quantityIsValid,
+  locationIsValid,
+  conditionsIsValid;
 
-// get last value
-const lastValue = document.getElementById("last").value;
+// fonction pour vérifier la validité de tous les input et afficher les messaqges d'erreur
+function testInputsValidity() {
+  for (let index = 0; index < inputsIds.length; index++) {
+    // stockage du nom de l'id (ex: first)
+    const inputId = inputsIds[index].id;
 
-// get last value length
-const lastValueLength = document.getElementById("last").value.length;
+    // stockage du message d'erreur correspondant (ex: Veuillez entrer 2 caractères)
+    const inputErrorMessage = inputsIds[index].errorMessage;
 
-// check if last value length is valid
-const lastIsValid = lengthIsLongEnough(lastValueLength);
+    // stockage de la valeur saisie par l'utilisateur (ex John)
+    let inputValue = getValue(inputId);
 
-// get email value
-const emailValue = document.getElementById("email").value;
+    // switch des differents inputs
+    switch (inputId) {
+      case "first":
+        // enlève l'eventuel message d'erreur
+        hideError(first.closest(".formData"));
+        // verifie la condition de validité de la donnée
+        if (inputValue.length >= 2) {
+          // fait passer la variable sur true
+          firstIsValid = true;
+        } else {
+          console.log("** error  in " + inputId);
+          // fait passer al variable sur true
+          firstIsValid = false;
+          // affiche le message d'erreur sur l'input
+          showError(first.closest(".formData"), inputErrorMessage);
+          // TODO trouver comment remplacer first par la variable inputId pour grouper les case first et last
+        }
+        break;
 
-// check if email is valid
-function emailChecker(email) {
-  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-}
-const emailIsValid = emailChecker(emailValue);
+      case "last":
+        // enlève l'eventuel message d'erreur
+        hideError(last.closest(".formData"));
+        // verifie la condition de validité de la donnée
+        if (inputValue.length >= 2) {
+          // fait passer la variable sur true
+          lastIsValid = true;
+        } else {
+          // fait passer la variable sur false
+          lastIsValid = false;
+          // affiche le message d'erreur sur l'input
+          showError(last.closest(".formData"), inputErrorMessage);
+        }
+        break;
 
-// get birthdate
-const birthdateValue = document.getElementById("birthdate").value;
+      case "email":
+        // enlève l'eventuel message d'erreur
+        hideError(email.closest(".formData"));
+        // verifie la condition de validité de la donnée
+        if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(inputValue)) {
+          // fait passer la variable sur true
+          emailIsValid = true;
+        } else {
+          // fait passer la variable sur false
+          emailIsValid = false;
+          // affiche le message d'erreur sur l'input
+          showError(email.closest(".formData"), inputErrorMessage);
+        }
+        break;
 
-// check if birthdate is valid
-function birthdateChecker(birthdate) {
-  return /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(
-    birthdate
-  );
-}
-const birthdateIsValid = birthdateChecker(birthdateValue);
+      case "birthdate":
+        // enlève l'eventuel message d'erreur
+        hideError(birthdate.closest(".formData"));
+        // verifie la condition de validité de la donnée
+        if (
+          /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(
+            inputValue
+          )
+        ) {
+          // fait passer la variable sur true
+          birthdateIsValid = true;
+        } else {
+          // fait passer la variable sur false
+          birthdateIsValid = false;
+          // affiche le message d'erreur sur l'input
+          showError(birthdate.closest(".formData"), inputErrorMessage);
+        }
+        break;
 
-// get quantity
-const quantityValue = document.getElementById("quantity").value;
+      case "quantity":
+        // enlève l'eventuel message d'erreur
+        hideError(quantity.closest(".formData"));
+        // verifie la condition de validité de la donnée
+        if (/^\d+$/.test(inputValue)) {
+          // fait passer la variable sur true
+          quantityIsValid = true;
+        } else {
+          // fait passer la variable sur false
+          quantityIsValid = false;
+          // affiche le message d'erreur sur l'input
+          showError(quantity.closest(".formData"), inputErrorMessage);
+        }
+        break;
 
-// check if quantity is a number
-function numberChecker(string) {
-  return /^\d+$/.test(string);
-}
-const quantityIsValid = numberChecker(quantityValue);
+      case "location1":
+        // enlève l'eventuel message d'erreur
+        hideError(location1.closest(".formData"));
+        // verifie la condition de validité de la donnée
+        // TODO check why if else don't work / locationIsValid is always true
+        if (document.querySelector('input[name="location"]:checked')) {
+          // fait passer la variable sur true
+          locationIsValid = true;
+        } else {
+          // fait passer la variable sur false
+          locationIsValid = false;
+          // affiche le message d'erreur sur l'input
+          showError(location1.closest(".formData"), inputErrorMessage);
+        }
+        break;
 
-// location is checked
-function locationChecker() {
-  return !document.querySelector('input[name="location"]:checked')
-    ? false
-    : true;
-}
-
-// check if location is checked
-const locationIsValid = locationChecker();
-
-// TODO verify why it doesn't work with an anonymous function
-// const locationIsValid2 = function () {
-//   return !document.querySelector('input[name="location"]:checked')
-//     ? false
-//     : true;
-// };
-
-// conditions is checked
-function conditionsChecker() {
-  return !document.querySelector('input[id="checkbox1"]:checked')
-    ? false
-    : true;
-}
-
-// check if conditions is checked
-const conditionsIsValid = conditionsChecker();
-
-// TODO prevent modal close and save data if form is unvalid
-
-// ------------------------------------------
-
-// ------------------------------------------------
-// issue#3: Ajouter validation ou messages d'erreur
-// ------------------------------------------------
-// pour chaque input si checkInput est faux afficher la pseudo class after corespondante
-const formInputs = {
-  first: [
-    firstIsValid,
-    "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
-  ],
-  last: [
-    lastIsValid,
-    "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
-  ],
-  email: [emailIsValid, "Veuillez entrer une adresse e-mail valide."],
-  birthdate: [birthdateIsValid, "Vous devez entrer votre date de naissance."],
-  quantity: [quantityIsValid, "Veuillez entrer un nombre."],
-  location: [locationIsValid, "Vous devez choisir une option."],
-  conditions: [
-    conditionsIsValid,
-    "Vous devez accepter les termes et conditions.",
-  ],
-};
-
-function showErrorMessage(inputName) {
-  if (!formInputs[inputName][0]) {
-    // ajouter formInputs.name[1] au content du after correspondant
-    console.log("input: " + inputName);
-    console.log(formInputs[inputName][1]);
+      case "checkbox1":
+        // enlève l'eventuel message d'erreur
+        hideError(checkbox1.closest(".formData"));
+        // verifie la condition de validité de la donnée
+        // TODO check why if else don't work / conditionsIsValid is always undefined
+        if (document.querySelector('input[id="checkbox1"]:checked')) {
+          // fait passer la variable sur true
+          locationIsValid = true;
+        } else {
+          // fait passer la variable sur false
+          locationIsValid = false;
+          // affiche le message d'erreur sur l'input
+          showError(checkbox1.closest(".formData"), inputErrorMessage);
+        }
+        break;
+    }
   }
-}
 
-// const formData = document.querySelectorAll(".formData");
+  // verifier si tous les inputs sont valides
+  let formIsValid =
+    firstIsValid &&
+    lastIsValid &&
+    emailIsValid &&
+    birthdateIsValid &&
+    quantityIsValid &&
+    locationIsValid &&
+    conditionsIsValid;
 
-// check each input looking for error
-function checkForError() {
-  for (const [key, value] of Object.entries(formInputs)) {
-    showErrorMessage(key);
+  // valide le formulaire s'il est valide
+  if (formIsValid) {
+    console.log("*** Formulaire valide ! ***");
+    // efface le formulaire
+    form.reset();
+    // ferme la modale
+    closeModal();
+    // affiche la fenetre de confirmation d'inscription
+    setTimeout(() => {
+      openConfirmation();
+    }, 100);
+  } else {
+    console.log("*** Formulaire invalide ! ***");
   }
+
+  console.log("first is valid ?", firstIsValid);
+  console.log("last is valid ?", lastIsValid);
+  console.log("email is valid ?", emailIsValid);
+  console.log("birthdate is valid ?", birthdateIsValid);
+  console.log("quantity is valid ?", quantityIsValid);
+  console.log("location is valid ?", locationIsValid);
+  console.log("conditions is valid ?", conditionsIsValid);
 }
 
-// ------------------------------------------------
-
-// ------------
-// testing mode
-// ------------
-const testingBtn = document.querySelector(".testing");
-testingBtn.addEventListener("click", testingMode);
-function testingMode() {
-  // input tests below
-  // console.log("testingMode is on");
-  // console.log(first);
-  // console.log(firstValue);
-  // console.log(firstValue2);
-  // console.log("testingMode is off");
-  // console.log("testingMode is on");
-
-  // console.log("--- CHECK FIRST");
-  // console.log(firstValue.length);
-  // console.log(firstValueLength);
-
-  // console.log("--- CHECK LAST");
-  // console.log(lastValueLength);
-  // console.log(lengthIsLongEnough(lastValueLength));
-
-  // console.log("--- CHECK EMAIL");
-  // console.log(emailValue);
-  // console.log(emailChecker(emailValue));
-
-  // console.log(emailIsValid);
-  // console.log("testingMode is off");
-
-  // console.log("--- CHECK QUANTITY");
-  // console.log(quantityIsValid);
-  console.log(showErrorMessage(first));
+// fonction d'ouverture de la fenetre de confirmation d'inscription
+function openConfirmation() {
+  confirmationbg.style.display = "block";
 }
 
-// ------------
+// DOM Elements
+const confirmationbg = document.querySelector(".bground-confirmation");
+const closeConfirmationBtn = document.querySelector(".close-confirmation");
+const submitConfirmationBtn = document.querySelector(
+  ".btn-submit-confirmation"
+);
+
+// event click sur bouton close (X et Fermer)
+closeConfirmationBtn.addEventListener("click", closeConfirmation);
+submitConfirmationBtn.addEventListener("click", closeConfirmation);
+
+// fonction de fermeture de la fenetre de confirmation
+function closeConfirmation() {
+  confirmationbg.style.display = "none";
+}
